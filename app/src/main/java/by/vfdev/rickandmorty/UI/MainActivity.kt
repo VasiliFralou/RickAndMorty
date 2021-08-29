b308f078072ea12e61bb4ad3484c449d0d98d6ad
Vasili <frolov98blr@mail.ru>
2021-08-29 21:02:58 +0300

    Update to 0.0.2 (2)

--- app/src/main/java/by/vfdev/rickandmorty/UI/MainActivity.kt
+++ app/src/main/java/by/vfdev/rickandmorty/UI/MainActivity.kt
@@ -2,11 +2,27 @@ package by.vfdev.rickandmorty.UI
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
 import by.vfdev.rickandmorty.R
+import by.vfdev.rickandmorty.ViewModel.CharacterViewModel
+import by.vfdev.rickandmorty.ViewModel.CharacterViewModelFactory
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
+    lateinit var viewModel: CharacterViewModel
+
+    @Inject lateinit var factory: CharacterViewModelFactory
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        viewModel = ViewModelProvider(this, factory).get(CharacterViewModel::class.java)
     }
 }
